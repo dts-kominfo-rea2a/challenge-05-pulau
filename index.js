9b38cf57df3aed6887a6c1d0b30b4cae75b4fb14
pulau <muchammad.anas@gmail.com>
2022-06-05 19:51:04 +0700

    09 - JS Callback - Tantangan

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (list, callback) => {
+  listName = callback(list);
+  let data = [];
+  for (let i = 0; i < listName.length; i++) {
+    data.push(`${i + 1}. ${listName[i]}`);
+  }
+  return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr) => {
+  return arr.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+  return arr.sort().reverse();
+};
+
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
